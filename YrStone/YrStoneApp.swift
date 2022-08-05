7c5871bc0f3a12babd112f7fa23edba4c5d817c7
yiran.feng <fangyira@gmail.com>
2022-08-05 14:28:28 +0900

    Initial Commit

---
+++ YrStone/YrStoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YrStoneApp.swift
+//  YrStone
+//
+//  Created by Yiran Feng on 2022/08/05.
+//
+
+import SwiftUI
+
+@main
+struct YrStoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
